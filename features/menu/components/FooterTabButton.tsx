d1559bb9f16df5daf9dfb1b4eb17a22b19bd2cf5
andycloke <apcloke@gmail.com>
2018-05-20 16:22:19 +0100

    Move tabs to bottom of menu

---
+++ features/menu/components/FooterTabButton.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button, Text, FooterTab } from 'native-base';
+
+type Props = {
+  onPress: (e: any) => void;
+  text: string;
+  active: boolean;
+};
+
+const FooterTabButton = ({ onPress, text, active }: Props) => (
+  <FooterTab>
+    <Button onPress={onPress}>
+      <Text style={active ? styles.textHighlighted : null}>{text}</Text>
+    </Button>
+  </FooterTab>
+);
+
+export default FooterTabButton;
+
+const styles = StyleSheet.create({
+  textHighlighted: {
+    color: 'blue'
+  }
+});
--- features/menu/components/MainMenu.tsx
+++ features/menu/components/MainMenu.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
-import {
-  Content,
-  Tab,
-  Tabs,
-  Drawer
-} from 'native-base';
+import { Content, Drawer, Button, Text, FooterTab, Footer } from 'native-base';
 
 import PeopleMenu from '../features/people/containers/PeopleMenu';
 import TensesMenu from '../features/tenses/containers/TensesMenu';
@@ -12,8 +7,16 @@ import VerbsMenu from '../features/verbs/containers/VerbsMenu';
 
 import MainMenuHeader from './MainMenuHeader';
 import Sidebar from './Sidebar';
+import FooterTabButton from './FooterTabButton';
 
-class MainMenu extends React.PureComponent<void> {
+type State = {
+  activeTabIndex: number;
+};
+
+class MainMenu extends React.PureComponent<void, State> {
+  state = {
+    activeTabIndex: 1
+  };
   drawer: any;
   closeDrawer = () => {
     this.drawer._root.close();
@@ -21,7 +24,13 @@ class MainMenu extends React.PureComponent<void> {
   openDrawer = () => {
     this.drawer._root.open();
   };
+  makeChangeTab = (index: number) => (e: any) =>
+    this.setState({ activeTabIndex: index });
   render() {
+    const { activeTabIndex } = this.state;
+    const tab0Active = activeTabIndex === 0;
+    const tab1Active = activeTabIndex === 1;
+    const tab2Active = activeTabIndex === 2;
     return (
       <Drawer
         ref={ref => {
@@ -33,18 +42,27 @@ class MainMenu extends React.PureComponent<void> {
         <React.Fragment>
           <MainMenuHeader openDrawer={this.openDrawer} />
           <Content>
-            <Tabs initialPage={1}>
-              <Tab heading="People">
-                <PeopleMenu />
-              </Tab>
-              <Tab heading="Tenses">
-                <TensesMenu />
-              </Tab>
-              <Tab heading="Verbs">
-                <VerbsMenu />
-              </Tab>
-            </Tabs>
+            {tab0Active && <PeopleMenu />}
+            {tab1Active && <TensesMenu />}
+            {tab2Active && <VerbsMenu />}
           </Content>
+          <Footer>
+            <FooterTabButton
+              onPress={this.makeChangeTab(0)}
+              text="People"
+              active={tab0Active}
+            />
+            <FooterTabButton
+              onPress={this.makeChangeTab(1)}
+              text="Tenses"
+              active={tab1Active}
+            />
+            <FooterTabButton
+              onPress={this.makeChangeTab(2)}
+              text="Verbs"
+              active={tab2Active}
+            />
+          </Footer>
         </React.Fragment>
       </Drawer>
     );
